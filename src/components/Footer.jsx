76d60d70368fb03b8240bcf8c30297cf10867214
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-06 19:50:07 +0800

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,16 +10,16 @@ import logo from '../assets/logos/logo.svg';
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="d-flex flex-column flex-lg-row justify-content-between">
-        <img src={logo} alt="Boulder Bike Tour logo" className="logo" />
-        <div>
-          <h5 className="mb-3"><strong>BBT Sport center</strong></h5>
+      <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center align-items-lg-start">
+        <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-4 mb-lg-0" />
+        <div className="mb-4 mb-lg-0 text-center text-lg-start">
+          <h5 className="mb-2 mb-lg-3"><strong>BBT Sport center</strong></h5>
           <p className="mb-1">617 Cooper Avenue</p>
           <p className="mb-1">Aspen, CO 81611</p>
           <p className="mb-1">+1 (877) 611-2812</p>
         </div>
-        <div>
-          <h5 className="mb-3"><strong>Resources</strong></h5>
+        <div className="mb-4 mb-lg-0 text-center text-lg-start">
+          <h5 className="mb-2 mb-lg-3"><strong>Resources</strong></h5>
           <p className="mb-1">
             <NavLink exact="true" to="/contact" className="navlink">Contact</NavLink>
           </p>
@@ -30,8 +30,8 @@ const Footer = () => {
             <NavLink exact="true" to="/donate" className="navlink">Donate</NavLink>
           </p>
         </div>
-        <div>
-          <h5 className="mb-3"><strong>Partners</strong></h5>
+        <div className="mb-4 mb-lg-0 text-center text-lg-start">
+          <h5 className="mb-2 mb-lg-3"><strong>Partners</strong></h5>
           <p className="mb-1">
             <a href="https://www.redbull.com/" target="_blank">Red Bull</a>
           </p>
@@ -42,8 +42,8 @@ const Footer = () => {
             <a href="https://www.canyon.com/" target="_blank">Canyon</a>
           </p>
         </div>
-        <div>
-          <h5 className="mb-3"><strong>Follow us</strong></h5>
+        <div className="mb-4 mb-lg-0 text-center text-lg-start">
+          <h5 className="mb-2 mb-lg-3"><strong>Follow us</strong></h5>
           <FontAwesomeIcon icon="fa-brands fa-facebook-square" size="3x" className="navlink"/>
           <FontAwesomeIcon icon="fa-brands fa-instagram" size="3x" className="mx-3 navlink"/>
           <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="3x" className="navlink"/>
