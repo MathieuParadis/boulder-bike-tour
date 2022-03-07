1f344969a259a41bb65ab3720e69b94106378112
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-07 22:10:44 +0800

    edited hamburger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -27,15 +27,15 @@ const HamburgerMenu = () => {
         <NavLink className="menu-link my-3" exact="true" to="/" onClick={handleMenu}>Home</NavLink>
         <NavLink className="menu-link my-3" exact="true" to="/photos" onClick={handleMenu}>Photos</NavLink> 
         <NavLink className="menu-link my-3" exact="true" to="/riders" onClick={handleMenu}>Riders</NavLink>
-        <div className="my-3 py-3">
+        <div className="my-3 py-2">
           <a href="https://twitter.com/" target="_blank">
-            <FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" className="social-networks me-1 text-white"/>
+            <FontAwesomeIcon icon="fa-brands fa-twitter" size="3x" className="social-networks me-1 text-white"/>
           </a>
           <a href="https://facebook.com/" target="_blank">
-            <FontAwesomeIcon icon="fa-brands fa-facebook-f" size="2x" className="social-networks mx-3 text-white"/>
+            <FontAwesomeIcon icon="fa-brands fa-facebook-f" size="3x" className="social-networks mx-3 text-white"/>
           </a>
           <a href="https://instagram.com/" target="_blank">
-            <FontAwesomeIcon icon="fa-brands fa-instagram" size="2x" className="social-networks ms-1 text-white"/>
+            <FontAwesomeIcon icon="fa-brands fa-instagram" size="3x" className="social-networks ms-1 text-white"/>
           </a>
         </div>
         <NavLink exact="true" to="/contest" className="contest-button h4 my-3 p-3 text-white text-center">Join contest</NavLink>
