21b056b7211699496e0dfa3e87941ea23d14c548
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-07 22:02:59 +0800

    edited hamburger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,11 +2,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const HamburgerMenu = () => {
   const handleMenu = () => {
     document.querySelector(".span1").classList.toggle("clicked");
     document.querySelector(".span2").classList.toggle("clicked");
     document.querySelector(".span3").classList.toggle("clicked");
+    document.querySelector(".overlay").classList.toggle("clicked");
     document.querySelector(".menu").classList.toggle("clicked");
     document.querySelector("body").classList.toggle("clicked");
   };
@@ -18,11 +22,23 @@ const HamburgerMenu = () => {
         <span className="span2"></span>
         <span className="span3"></span>
       </div>
+      <div className="overlay"></div>
       <div className="menu">
-        <NavLink className="menu-link" exact="true" to="/" onClick={handleMenu}>Home</NavLink>
-        {/* <NavLink className="menu-link" exact="true" to="/portfolio" onClick={handleMenu}>Portfolio</NavLink>
-        <NavLink className="menu-link" exact="true" to="/resume" onClick={handleMenu}>Resume</NavLink>
-        <NavLink className="menu-link" exact="true" to="/contact" onClick={handleMenu}>Contact</NavLink> */}
+        <NavLink className="menu-link my-3" exact="true" to="/" onClick={handleMenu}>Home</NavLink>
+        <NavLink className="menu-link my-3" exact="true" to="/photos" onClick={handleMenu}>Photos</NavLink> 
+        <NavLink className="menu-link my-3" exact="true" to="/riders" onClick={handleMenu}>Riders</NavLink>
+        <div className="my-3 py-3">
+          <a href="https://twitter.com/" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" className="social-networks me-1 text-white"/>
+          </a>
+          <a href="https://facebook.com/" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-facebook-f" size="2x" className="social-networks mx-3 text-white"/>
+          </a>
+          <a href="https://instagram.com/" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-instagram" size="2x" className="social-networks ms-1 text-white"/>
+          </a>
+        </div>
+        <NavLink exact="true" to="/contest" className="contest-button h4 my-3 p-3 text-white text-center">Join contest</NavLink>
       </div>
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,8 @@
 // CONFIG IMPORTS
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+// REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // COMPONENTS IMPORTS
@@ -11,33 +13,36 @@ import logo from '../assets/logos/logo_name.svg';
 
 const Navigation = () => {
   return (
-    <div className="navigation">
+    <>
       <HamburgerMenu />
-      <div className="d-flex justify-content-between align-items-center">
-        <div className="d-flex align-items-center">
-          <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-0 me-4 me-xl-5" />
-          <div className="d-none d-lg-flex">
-            <NavLink exact="true" to="/" className="navlink h4 me-2 me-xl-4 mb-0">Home</NavLink>
-            <NavLink exact="true" to="/photos" className="navlink h4 mx-2 mx-xl-4 mb-0">Photos</NavLink>
-            <NavLink exact="true" to="/riders" className="navlink h4 ms-2 ms-xl-4 mb-0">Riders</NavLink>
+      <div className="navigation">
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-0 me-4 me-xl-5" />
+            <div className="d-none d-lg-flex">
+              <NavLink exact="true" to="/" className="navlink h4 me-2 me-xl-4 mb-0">Home</NavLink>
+              <NavLink exact="true" to="/photos" className="navlink h4 mx-2 mx-xl-4 mb-0">Photos</NavLink>
+              <NavLink exact="true" to="/riders" className="navlink h4 ms-2 ms-xl-4 mb-0">Riders</NavLink>
+            </div>
           </div>
-        </div>
-        <div className="d-none d-lg-flex align-items-center">
-          <div>
-            <a href="https://twitter.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" className="social-networks me-1"/>
-            </a>
-            <a href="https://facebook.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-facebook-f" size="2x" className="social-networks mx-3"/>
-            </a>
-            <a href="https://instagram.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-instagram" size="2x" className="social-networks ms-1"/>
-            </a>
+          <div className="d-none d-lg-flex align-items-center">
+            <div>
+              <a href="https://twitter.com/" target="_blank">
+                <FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" className="social-networks me-1"/>
+              </a>
+              <a href="https://facebook.com/" target="_blank">
+                <FontAwesomeIcon icon="fa-brands fa-facebook-f" size="2x" className="social-networks mx-3"/>
+              </a>
+              <a href="https://instagram.com/" target="_blank">
+                <FontAwesomeIcon icon="fa-brands fa-instagram" size="2x" className="social-networks ms-1"/>
+              </a>
+            </div>
+            <NavLink exact="true" to="/contest" className="contest-button h4 ms-4 ms-xl-5 p-3 mb-0 text-white text-center">Join contest</NavLink>
           </div>
-          <NavLink exact="true" to="/contest" className="contest-button h4 ms-4 ms-xl-5 p-3 mb-0 text-white text-center">Join contest</NavLink>
         </div>
       </div>
-    </div>
+    </>
+    
   );
 };
 
