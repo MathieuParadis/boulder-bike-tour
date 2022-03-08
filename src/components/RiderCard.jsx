cfb87a244579529ce8cff5849d1ca1dda6edc678
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-08 11:48:27 +0800

    edited rider card

--- src/components/RiderCard.jsx
+++ src/components/RiderCard.jsx
@@ -5,10 +5,8 @@ const RiderCard = ({rider}) => {
   const {first_name, last_name, city, state, img_url} = rider
   return (
     <div className="rider-card col-12 col-sm-6 col-md-4 col-lg-3 px-2 py-3">
-      <div className="card-front">
+      <div className="card-content">
         <img src={img_url} alt={first_name + " " + last_name + " photo"} />
-      </div>
-      <div className="card-back">
         <div className="overlay"></div>
         <div className="rider-info">
           <h3>{first_name + " " + last_name}</h3>
