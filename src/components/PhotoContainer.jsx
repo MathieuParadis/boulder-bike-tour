ac11a9732d853c1536619aae96133170ebf9e818
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-11 21:38:46 +0800

    edited url on api call

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -4,7 +4,7 @@ const PhotoContainer = ({photo}) => {
   const {server, id, secret, title} = photo;
 
   return (
-    <div className="photo-container col-12 col-md-6 col-lg-4 col-xl-3 px-2 py-3">
+    <div className="photo-container col-12 col-sm-6 col-md-4 col-lg-3">
       <div className="content">
         <img src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`} alt={title} key={id} />
       </div>
--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -8,12 +8,12 @@ const Photos = () => {
   const [photos, setPhotos] = React.useState(null);
 
   const getPhotosData = () => {
-    const url_base = "https://www.flickr.com/services/rest/";
-    const method = "flickr.photos.search";
-    const api_key = "1cfbe2c81c21a47e88cb0033b5fdc2dc";
-    const tags = ['bike', 'biking'];
-    const url = `${url_base}?method=${method}&api_key=${api_key}&tags=${tags[0]}%2C+${tags[1]}&per_page=40&format=json&nojsoncallback=1&auth_token=72157720835749557-09fa56a1f5066771&api_sig=014926c1bd49b4b0e3e9993667a20c1f`;
-  
+    const api_key = "02a2c3456e80bd16280dea2e4004e27b";
+    let page = 6;
+    // const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=mountain-bike%2C+bike%2C+race%2C+mountain%2C+colorado%2C&tag_mode=all&per_page=40&page=${page}&format=json&nojsoncallback=1&auth_token=72157720835761134-f7d2ed54f9d9c21b&api_sig=b8d8f4ab9da81ca4b502eff15120e630`
+
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=mountain-bike%2C+bike%2C+race%2C+mountain%2C+colorado%2C&tag_mode=all&per_page=40&page=${page}&format=json&nojsoncallback=1`
+
     fetch(url, {
       method: "GET",
     })
@@ -33,17 +33,17 @@ const Photos = () => {
   
   return (
     <div className="photos">
-      <div className="photos-container">
-        <div className="d-flex flex-wrap justify-content-between align-items-center">
-        { photos &&
-          photos.map((photo) => {
-            return (
-              <PhotoContainer photo={photo} key={photo.id} />
-            )
-          })
-        }
+      { photos &&
+        <div className="photos-container d-flex flex-wrap justify-content-between align-items-center">
+          {
+            photos.map((photo) => {
+              return (
+                <PhotoContainer photo={photo} key={photo.id} />
+              )
+            })
+          }
         </div>
-      </div>
+      }
     </div>
   );
 };
