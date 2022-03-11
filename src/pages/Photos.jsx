318e28fc5187d11a9ed3a4dde0fe7be33d961b90
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-11 22:50:47 +0800

    edited photo page: added the change page option

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -5,16 +5,21 @@ import React, {useEffect, useState} from 'react';
 import PhotoContainer from '../components/PhotoContainer';
 
 const Photos = () => {
-  const [photos, setPhotos] = React.useState(null);
+  let page_number = 0;
 
-  const getPhotosData = () => {
+  const [photos, setPhotos] = React.useState(null);
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const [totalPages, setTotalPages] = React.useState(0);
+  const [page, setPage] = React.useState(page_number);
+
+
+  const getPhotosData = (page) => {
     const base_url = 'https://www.flickr.com/services/rest/';
     const method = 'flickr.photos.search';
     const api_key = "02a2c3456e80bd16280dea2e4004e27b";
     const tags_array = ['mountain-bike', 'bike', 'race', 'colorado', 'mountain'];
     const tags = `${tags_array[0]}%2C+${tags_array[1]}%2C+${tags_array[2]}%2C+${tags_array[3]}%2C+${tags_array[4]}%2C`;
     const per_page = 40;
-    let page = 1;
 
     const url = `${base_url}?method=${method}&api_key=${api_key}&tags=${tags}&tag_mode=all&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`
 
@@ -28,25 +33,41 @@ const Photos = () => {
     .then(response => {
         console.log(response);
         setPhotos(response.photos.photo);
+        setCurrentPage(response.photos.page);
+        setTotalPages(response.photos.pages);
     });
   }
+  
+  const displayNextPage = () => {
+    // page_number += 1;
+    setPage(page + 1);
+    // getPhotosData(page_number);
+    // alert(page_number);
+    // getPhotosData(12);
+  }
 
-  useEffect(() => {
-    getPhotosData();
-  }, []);
+  useEffect(() => { 
+    getPhotosData(page);
+  }, [page]);
   
   return (
-    <div className="photos">
-      { photos &&
-        <div className="photos-container d-flex flex-wrap justify-content-between align-items-center">
-          {
-            photos.map((photo) => {
-              return (
-                <PhotoContainer photo={photo} key={photo.id} />
-              )
-            })
-          }
-        </div>
+    <div className="photos py-3">
+      { photos && photos.length > 0 &&
+        <>
+          <div className="photos-container d-flex flex-wrap justify-content-between align-items-center">
+            {
+              photos.map((photo) => {
+                return (
+                  <PhotoContainer photo={photo} key={photo.id} />
+                )
+              })
+            }
+          </div>
+          <button onClick={() => displayNextPage()}>Next page</button>      
+          Page: {currentPage} / {totalPages}
+          Page: {page} / {totalPages}
+
+        </>
       }
     </div>
   );
