be50016e2084e1555a73b965c4458e31be5a2bfa
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-13 23:15:42 +0800

    edited box section in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // CONFIG IMPORTS
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 // ASSETS IMPORTS
 import homepage_bg from '../assets/images/homepage_bg.jpeg';
@@ -14,14 +15,25 @@ const Home = () => {
       </div>
       <div className="box-section">
         <div className="colored-bg d-flex justify-content-center align-items-center">
-          <div className="boxes d-flex flex-column flex-md-row justify-content-between align-items-center">
-            <div className="box col-12 col-sm-6 col-md-4 p-3">
-              <div className="box-content">
-                <img src="" alt="" className="img-box" />
-              </div>
-            </div>
-            <div className="box col-12 col-sm-6 col-md-4 p-3">div1</div>
-            <div className="box col-12 col-sm-6 col-md-4 p-3">div1</div>
+          <div className="boxes d-flex flex-wrap flex-row justify-content-center align-items-center">
+            <NavLink exact="true" to="/race" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
+                <div className="box-content d-flex flex-column justify-content-center align-items-center">
+                  <img src={homepage_bg} alt="" className="img-box" />
+                  <h3 className="text-center">Race<br></br>Information</h3>
+                </div>
+            </NavLink>
+            <NavLink exact="true" to="/area" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
+                <div className="box-content d-flex flex-column justify-content-center align-items-center">
+                  <img src={homepage_bg} alt="" className="img-box" />
+                  <h3 className="text-center">About the<br></br>Area</h3>
+                </div>
+            </NavLink>
+            <NavLink exact="true" to="/volunteer" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
+                <div className="box-content d-flex flex-column justify-content-center align-items-center">
+                  <img src={homepage_bg} alt="" className="img-box" />
+                  <h3 className="text-center">Become a<br></br>Volunteer</h3>
+                </div>
+            </NavLink>
           </div>
         </div>
       </div>
