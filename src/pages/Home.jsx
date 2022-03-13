acdff8629db3a3d01e4cacd9b349e863d2e0e29c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-13 23:34:45 +0800

    done editing box section in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 // ASSETS IMPORTS
-import homepage_bg from '../assets/images/homepage_bg.jpeg';
+import race from '../assets/images/race_illustration.jpeg';
+import area from '../assets/images/area_illustration.jpg';
+import volunteer from '../assets/images/volunteer illustration.jpeg';
 
 const Home = () => {
 
@@ -18,19 +20,19 @@ const Home = () => {
           <div className="boxes d-flex flex-wrap flex-row justify-content-center align-items-center">
             <NavLink exact="true" to="/race" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
                 <div className="box-content d-flex flex-column justify-content-center align-items-center">
-                  <img src={homepage_bg} alt="" className="img-box" />
+                  <img src={race} alt="race illustration" className="img-box" />
                   <h3 className="text-center">Race<br></br>Information</h3>
                 </div>
             </NavLink>
             <NavLink exact="true" to="/area" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
                 <div className="box-content d-flex flex-column justify-content-center align-items-center">
-                  <img src={homepage_bg} alt="" className="img-box" />
+                  <img src={area} alt="area illustration" className="img-box" />
                   <h3 className="text-center">About the<br></br>Area</h3>
                 </div>
             </NavLink>
             <NavLink exact="true" to="/volunteer" className="box d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-4 p-3">
                 <div className="box-content d-flex flex-column justify-content-center align-items-center">
-                  <img src={homepage_bg} alt="" className="img-box" />
+                  <img src={volunteer} alt="volunteer illustration" className="img-box" />
                   <h3 className="text-center">Become a<br></br>Volunteer</h3>
                 </div>
             </NavLink>
