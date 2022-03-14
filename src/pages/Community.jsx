4600b9d63583340f22a696e2808bd3939dc09721
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-15 07:10:51 +0800

    edited community page

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -16,7 +16,21 @@ const Community = () => {
   return (
     <div className="community">
       <Banner img={community_banner} title="Community" />
-
+      <div className="community-content">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <h3 className="mb-4">Sport, a vector of integration</h3>
+          <p>
+            BBT Sport Center is a non-profit organization created by our admiration for the sport of cycling. Bonded by friendship, we are passionately committed to serving our community and promoting the etiquette and safety of cycling to young riders.
+            Using cycling as a springboard, we extend the organization’s reach through our active involvement with local charities. All year long we organize events to promote cycling and sport in general, to underprivileged people and communities.
+          </p>
+          <p>
+            Our Partnership with the Clark County School District has our members teaching your youth biking safety and the joy of riding a bike. With Bike Rodeos and Safe Routes to School programs, 
+            our Team members are able to share and teach our local youth their passion of riding a bike. With the help of our sponsors, teammates, and donations we are able to provide bikes, helmets, 
+            bike locks and various other services to local children who may not otherwise obtain these items.
+          </p>
+          <q className="h5">Sport has a unique power to bring people together, attract, mobilize, inspire and generate social inclusion.</q>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -20,8 +20,11 @@ const Volunteer = () => {
           <h3 className="mb-4">Volunteering, what is it?</h3>
           <p>Becoming a volunteer for the Boulder Bike Tour is first and foremost an opportunity to get involved in an association whose goal is to encounter nature and to surpass oneself.</p>
           <p>It is also an opportunity to discover the mountain landscapes, to meet other people and to support the men and women who will work all day to achieve their sporting challenge.</p>
-          <p>Whether you like logistics (setting up refreshment stands, managing bikes, organizing the site, preparing meals, etc) or you prefer to be on the ground (checking markers, taking note of times, securing dangerous points etc), there is bound to be something that suits you as a volunteer. 
-             And of course, all this happens in a joyful and good mood, always at least in pairs. So don't hesitate any longer and join the big family of the Boulder Bike Tour volunteers!</p>
+          <p>
+            Whether you like logistics (setting up refreshment stands, managing bikes, organizing the site, preparing meals, etc) or you prefer to be on the ground 
+            (checking markers, taking note of times, securing dangerous points etc), there is bound to be something that suits you as a volunteer. 
+            And of course, all this happens in a joyful and good mood, always at least in pairs. So don't hesitate any longer and join the big family of the Boulder Bike Tour volunteers!
+          </p>
         </div>
       </div>
     </div>
