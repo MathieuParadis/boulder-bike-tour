e21bd50cee96dfff2ec63b073f4f2a4eb149b87b
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-20 14:18:25 +0800

    edited homepage

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Countdown = () => {
   return (
-    <div className="countdown">
-      
+    <div className="countdown d-flex flex-wrap justify-content-center justify-content-md-end align-items-center">
+      <div className="d-flex flex-wrap justify-content-between align-items-center">
+        <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
+          <p className="mb-0">20</p>
+          <p className="mb-0">Days</p>
+        </div>
+        <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
+          <p className="mb-0">20</p>
+          <p className="mb-0">Hours</p>
+        </div>
+      </div>
+      <div className="d-flex flex-wrap justify-content-between align-items-center">
+        <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
+          <p className="mb-0">20</p>
+          <p className="mb-0">Minutes</p>
+        </div>
+        <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
+          <p className="mb-0">20</p>
+          <p className="mb-0">Seconds</p>
+        </div>
+      </div>
     </div>
   );
 };
