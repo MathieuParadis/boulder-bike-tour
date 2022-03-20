dc9ef4d30f9a908bd9eb6cc3dadf1cc166229abc
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-20 17:34:05 +0800

    edited countdown component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,11 +1,37 @@
 // CONFIG IMPORTS
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+import moment from 'moment'
 
 const Countdown = () => {
-  const [years, setYears] = useState(0);
+  const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+
+  const eventDate = "01/04/2022 00:00:00";
+
+  let secBeforeEvent = moment(eventDate, "DD/MM/YYYY HH:mm:ss").diff(moment().format())/1000;
+  let secCountdown = (secBeforeEvent) % 60;
+  let minCountdown = (Math.floor(secBeforeEvent / 60)) % 60;
+  let hoursCountdown = (Math.floor(secBeforeEvent / 3600)) % 24;
+  let daysCountdown = (Math.floor(secBeforeEvent / 86400));
+
+  console.log(secCountdown);
+  console.log(minCountdown);
+  console.log(hoursCountdown);
+  console.log(daysCountdown);
+
+  const updateCountdown = () => {
+    setSeconds(secCountdown);
+    setMinutes(minCountdown);
+    setHours(hoursCountdown);
+    setDays(daysCountdown);
+  }
+
+  useEffect(() => {
+    updateCountdown();
+  })
 
   return (
     <div className="countdown-section d-flex flex-column justify-content-md-end align-items-md-end col-12 col-md-6">
@@ -13,21 +39,21 @@ const Countdown = () => {
       <div className="countdown d-flex flex-wrap justify-content-center justify-content-md-end align-items-center">
         <div className="d-flex flex-wrap justify-content-between align-items-center">
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">20</p>
+            <p className="mb-0">{days}</p>
             <p className="mb-0">Days</p>
           </div>
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">20</p>
+            <p className="mb-0">{hours}</p>
             <p className="mb-0">Hours</p>
           </div>
         </div>
         <div className="d-flex flex-wrap justify-content-between align-items-center">
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">20</p>
+            <p className="mb-0">{minutes}</p>
             <p className="mb-0">Minutes</p>
           </div>
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">20</p>
+            <p className="mb-0">{seconds}</p>
             <p className="mb-0">Seconds</p>
           </div>
         </div>
