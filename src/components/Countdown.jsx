c6c3243e8682cb4a6e577c7712c23d860be19456
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-20 18:36:00 +0800

    edited style of countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -37,22 +37,22 @@ const Countdown = () => {
       <div className="countdown d-flex flex-wrap justify-content-center justify-content-md-end align-items-center">
         <div className="d-flex flex-wrap justify-content-between align-items-center">
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">{days}</p>
-            <p className="mb-0">Days</p>
+            <p className="countdown-text mb-0">{days}</p>
+            <p className="countdown-text mb-0">Days</p>
           </div>
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">{hours}</p>
-            <p className="mb-0">Hours</p>
+            <p className="countdown-text mb-0">{hours}</p>
+            <p className="countdown-text mb-0">Hours</p>
           </div>
         </div>
         <div className="d-flex flex-wrap justify-content-between align-items-center">
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">{minutes}</p>
-            <p className="mb-0">Minutes</p>
+            <p className="countdown-text mb-0">{minutes}</p>
+            <p className="countdown-text mb-0">Minutes</p>
           </div>
           <div className="time-square d-flex flex-column justify-content-center align-items-center m-2">
-            <p className="mb-0">{seconds}</p>
-            <p className="mb-0">Seconds</p>
+            <p className="countdown-text mb-0">{seconds}</p>
+            <p className="countdown-text mb-0">Seconds</p>
           </div>
         </div>
       </div>
