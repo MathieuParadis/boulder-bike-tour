3c42a85aaff91c545818fe6efaf0824c952b632a
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-20 18:31:28 +0800

    edited countdown component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -9,20 +9,15 @@ const Countdown = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  const eventDate = "01/04/2022 00:00:00";
-
-  let secBeforeEvent = moment(eventDate, "DD/MM/YYYY HH:mm:ss").diff(moment().format())/1000;
-  let secCountdown = (secBeforeEvent) % 60;
-  let minCountdown = (Math.floor(secBeforeEvent / 60)) % 60;
-  let hoursCountdown = (Math.floor(secBeforeEvent / 3600)) % 24;
-  let daysCountdown = (Math.floor(secBeforeEvent / 86400));
-
-  console.log(secCountdown);
-  console.log(minCountdown);
-  console.log(hoursCountdown);
-  console.log(daysCountdown);
+  const eventDate = "01/04/2022 09:00:00";
 
   const updateCountdown = () => {
+    let secBeforeEvent = moment(eventDate, "DD/MM/YYYY HH:mm:ss").diff(moment().format())/1000;
+    let secCountdown = (secBeforeEvent) % 60;
+    let minCountdown = (Math.floor(secBeforeEvent / 60)) % 60;
+    let hoursCountdown = (Math.floor(secBeforeEvent / 3600)) % 24;
+    let daysCountdown = (Math.floor(secBeforeEvent / 86400));
+
     setSeconds(secCountdown);
     setMinutes(minCountdown);
     setHours(hoursCountdown);
@@ -30,8 +25,11 @@ const Countdown = () => {
   }
 
   useEffect(() => {
-    updateCountdown();
-  })
+    const interval = setInterval(() => {
+      updateCountdown();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="countdown-section d-flex flex-column justify-content-md-end align-items-md-end col-12 col-md-6">
